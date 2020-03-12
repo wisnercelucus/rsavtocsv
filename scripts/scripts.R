95268166a318d7f7d4db72e90089d989cc88cfa0
wisnercelucus <wisnercelicus@gmail.com>
2020-03-12 16:59:55 -0400

    Add comments in the scripts.R file.

--- scripts/scripts.R
+++ scripts/scripts.R
@@ -4,14 +4,14 @@
 library(foreign)
 dataset <- read.spss("./data/sleep.sav", to.data.frame = T)
 
+#convert the named vector variable.labels to DataFrame
 dataset.labels <- as.data.frame(attr(dataset, "variable.labels")) 
 names(dataset.labels)
 
 colnames(dataset.labels)[1] <- "labels"
-labels <- dataset.labels$labels
-labels <-as.character(labels)
-labels
+labels <- as.character(dataset.labels$labels)
 
+#the swap function with swap not null labels with the questions code.
 swap_qc_l <- function(q_codes, q_labels){
   questions <- character()
   assertthat::are_equal(length(q_codes), length(q_labels))
@@ -25,14 +25,20 @@ swap_qc_l <- function(q_codes, q_labels){
   return(questions)
 }
 
+
+#Extract questions' codes
 question_codes <- as.character(colnames(dataset))
 
+#Call the swap_qc_l function
 questions <- swap_qn_l(question_codes,labels)
 
-
+#rename the columns names of the initial dataframe (dataset)
 colnames(dataset) <- questions
 
+#Verify that everything works perfectly
 View(dataset)
+
+#save the file to csv for exchange.
 write.csv(dataset, file="/path/sleep_corr.csv", row.names = F)
 
 
